131da63ab2a6cd2c0686a595ad10c66fb7a38689
nguyentct19 <thnguyen1928@gmail.com>
2025-02-26 09:14:04 +0700

    aaaaa

--- Furniture.API/Controllers/BaseApiController.cs
+++ Furniture.API/Controllers/BaseApiController.cs
@@ -6,28 +6,5 @@ namespace Furniture.API.Controllers;
 [ApiController]
 public class BaseApiController : ControllerBase
 {
-	protected ActionResult<PagedResult<T>> CreatePagedResult<T>(IEnumerable<T> list, int pageIndex, int pageSize)
-	{
-		int totalCount = list.Count();
-		var items = list.Skip((pageIndex - 1) * pageSize).Take(pageSize).ToList();
 
-		var result = new PagedResult<T>
-		{
-			Items = items,
-			PageIndex = pageIndex,
-			PageSize = pageSize,
-			TotalCount = totalCount,
-			TotalPages = (int)Math.Ceiling(totalCount / (double)pageSize)
-		};
-
-		return Ok(result);
-	}
-}
-public class PagedResult<T>
-{
-	public List<T> Items { get; set; } = new();
-	public int PageIndex { get; set; }
-	public int PageSize { get; set; }
-	public int TotalCount { get; set; }
-	public int TotalPages { get; set; }
 }
