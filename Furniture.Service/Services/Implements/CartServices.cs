f686c03617e3865119fab767b37148661c71936f
hoanghai1910 <142432287+hoanghai1910@users.noreply.github.com>
2025-02-27 10:12:01 +0700

    write api get orderItems

---
+++ Furniture.Service/Services/Implements/CartServices.cs
@@ -0,0 +1,18 @@
+﻿
+using Furniture.Core.Dtos.Cart;
+
+namespace Furniture.Service.Services.Implements;
+
+public class CartServices(ICartRepository cartRepository, ITokenService tokenService) : ICartServices
+{
+    public async Task<List<CartItemDto>> GetCartsAsync()
+    {
+
+        var account = await tokenService.Authenticate();
+        if (account != null)
+        {
+            return await cartRepository.GetCartProductsAsync(account.Id);
+        }
+        return null;
+    }
+}
---
+++ Furniture.Service/Services/Interfaces/ICartServices.cs
@@ -0,0 +1,8 @@
+﻿using Furniture.Core.Dtos.Cart;
+
+namespace Furniture.Service.Services.Interfaces;
+
+public interface ICartServices
+{
+    Task<List<CartItemDto>> GetCartsAsync();
+}
