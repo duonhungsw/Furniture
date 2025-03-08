448a0122efc052e2f137ee26d8a7ab821abf45a0
nguyentct19 <thnguyen1928@gmail.com>
2025-03-08 19:59:31 +0700

    ListAccountProductAndUpdateRole

--- Furniture.Web/Controllers/AdminController.cs
+++ Furniture.Web/Controllers/AdminController.cs
@@ -1,9 +1,16 @@
 ﻿namespace Furniture.Web.Controllers;
 
-public class AdminController : Controller
+public class AdminController(IProductApi _productApi) : Controller
 {
     public IActionResult Index()
     {
         return View();
+    }
+    public async Task<IActionResult> ProductList(int pageIndex = 1, int pageSize = 5)
+    {
+        var queryInfo = new QueryInfo { PageIndex = pageIndex, PageSize = pageSize };
+        var pagedResult = await _productApi.GetProductsWithPaging(queryInfo);
+
+        return View(pagedResult.Items);
     }
 }
