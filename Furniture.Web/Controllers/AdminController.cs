d3c25196cd2c5a5a1e96b8788885ce3f41527604
nguyentct19 <thnguyen1928@gmail.com>
2025-03-08 22:19:40 +0700

    fix

--- Furniture.Web/Controllers/AdminController.cs
+++ Furniture.Web/Controllers/AdminController.cs
@@ -104,11 +104,5 @@ public class AdminController(IProductApi _productApi) : Controller
             return RedirectToAction("ProductList");
         ModelState.AddModelError("", "Delete Failed.");
         return RedirectToAction("ProductList");
-=======
-        var queryInfo = new QueryInfo { PageIndex = pageIndex, PageSize = pageSize };
-        var pagedResult = await _productApi.GetProductsWithPaging(queryInfo);
-
-        return View(pagedResult.Items);
->>>>>>> Stashed changes
     }
 }
