3e025321930cd8fbf18f41fb5b7d7691e3bfcbcb
hoanghai1910 <142432287+hoanghai1910@users.noreply.github.com>
2025-03-12 08:54:55 +0700

    Update quantity,status, and delete cart Item

--- Furniture.Web/Controllers/CartController.cs
+++ Furniture.Web/Controllers/CartController.cs
@@ -1,12 +1,53 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿namespace Furniture.Web.Controllers;
 
-namespace Furniture.Web.Controllers;
-
-public class CartController : Controller
+public class CartController(ICartApi cartApi, IAccountApi accountApi) : Controller
 {
     [HttpGet]
-    public IActionResult ShoppingCart()
+    public async Task<IActionResult> ShoppingCart()
     {
-        return View();
+        var account = await accountApi.GetUserInfoAsync();
+        var response = await cartApi.GetCarts(account!.Content!.Id);
+
+        if (!response.IsSuccessStatusCode || response.Content == null)
+        {
+            return View(new List<CartItemDto>()); // Nếu lỗi, trả về danh sách rỗng
+        }
+
+        return View(response.Content); // Chỉ truyền danh sách CartItemDto vào View
     }
+    [HttpPatch]
+    public async Task<bool> UpdateCartItemByQuantity([FromBody] CartUpdateQuantityDto model)
+    {
+        var account = await accountApi.GetUserInfoAsync();
+        if (account != null)
+        {
+            var response = await cartApi.UpdateCartItemByQuantity(model);
+            return response;
+        }
+        return false;
+    }
+    [HttpPatch]
+    public async Task<bool> UpdateCartItemByStatus([FromRoute] CartItemDto model)
+    {
+        var account = await accountApi.GetUserInfoAsync();
+        if (account != null)
+        {
+            var result = await cartApi.UpdateCartItemByStatus(model.Id);
+            return result;
+        }
+        return false;
+    }
+    [HttpDelete]
+    public async Task<bool> DeleteCartItem([FromRoute] CartItemDto model)
+    {
+        var account = await accountApi.GetUserInfoAsync();
+        if (account != null)
+        {
+            bool result = await cartApi.DeleteCartItem(model.Id);
+            return result;
+        }
+        return false;
+    }
+
+
 }
