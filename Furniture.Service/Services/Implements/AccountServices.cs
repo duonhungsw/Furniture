7129cde58741d07168d00ae466b3f9cd4816f898
nguyentct19 <thnguyen1928@gmail.com>
2025-03-15 01:15:16 +0700

    Fix merge conflict in AccountServices.cs and Update.cshtml

--- Furniture.Service/Services/Implements/AccountServices.cs
+++ Furniture.Service/Services/Implements/AccountServices.cs
@@ -1,72 +1,61 @@
-<<<<<<< HEAD
-=======
 
-﻿using Furniture.Core;
->>>>>>> 676201f819531f9ef0262eb1997954c9475d276d
+using Furniture.Core;
 using Microsoft.AspNetCore.Mvc;
 namespace Furniture.Service;
 
 public class AccountServices(
-<<<<<<< HEAD
-    IAccountRepository _repository,
-    ITokenService _tokenService,
-    IFileStorageService _fileStorageService,
-    IMapper _mapper) : IAccountServices
-=======
 	IAccountRepository _repository,
 	ITokenService _tokenService,
 	IFileStorageService _fileStorageService,
 	ICartRepository _cartRepository,
 	IMapper _mapper) : IAccountServices
->>>>>>> 4f09a67695f7b2273ebc1399972cf08836ee3b3a
 {
-    private readonly string accountContainer = ContainerName.account.ToString();
-    public Task<bool> ChangePasswordAsync(ChangePasswordDto changePasswordDto)
-    {
-        throw new NotImplementedException();
-    }
-    public async Task<AccountDto> GetAccountByEmailAsync(string Email)
-    {
+	private readonly string accountContainer = ContainerName.account.ToString();
+	public Task<bool> ChangePasswordAsync(ChangePasswordDto changePasswordDto)
+	{
+		throw new NotImplementedException();
+	}
+	public async Task<AccountDto> GetAccountByEmailAsync(string Email)
+	{
 
-        var account = await _repository.GetByEmailAsync(Email);
-        var result = _mapper.Map<AccountDto>(account);
-        return result;
-    }
-    public async Task<AccountDto> GetAccountByIdAsync(Guid Id)
-    {
-        var account = await _repository.GetByIdAsync(Id);
-        if (account == null)
-            throw new NotFoundException(ErrorMessageBase.Format(ErrorMessageBase.NotFound, "Account", Id));
-
-
-        var result = _mapper.Map<AccountDto>(account);
-        return result;
-    }
-
-    public Task<AccountDto> GetAccountByPasswordAsync(Guid id, string password)
-    {
-        throw new NotImplementedException();
-    }
-
-    public async Task<Guid?> GetAccountIdAsync()
-    {
-        var account = await _tokenService.GetTokenAsync();
-        return account?.Id;
-    }
-    public async Task<List<AccountDto>> GetAccountsAsync()
-        => await _repository.GetAccountsAsync();
-
-    public async Task<TokenResponse?> LoginAsync(SignInDTOs model)
-    {
-        model.HashPassword = PasswordHasher.HashPasswordPBKDF2(model.HashPassword!);
-        var account = _mapper.Map<Account>(model);
-
-        var result = await _repository.LoginAsync(account);
-        if (result == null)
-            throw new NotFoundException(ErrorMessageBase.NotFound);
+		var account = await _repository.GetByEmailAsync(Email);
+		var result = _mapper.Map<AccountDto>(account);
+		return result;
+	}
+	public async Task<AccountDto> GetAccountByIdAsync(Guid Id)
+	{
+		var account = await _repository.GetByIdAsync(Id);
+		if (account == null)
+			throw new NotFoundException(ErrorMessageBase.Format(ErrorMessageBase.NotFound, "Account", Id));
 
 
-<<<<<<< Updated upstream
+		var result = _mapper.Map<AccountDto>(account);
+		return result;
+	}
+
+	public Task<AccountDto> GetAccountByPasswordAsync(Guid id, string password)
+	{
+		throw new NotImplementedException();
+	}
+
+	public async Task<Guid?> GetAccountIdAsync()
+	{
+		var account = await _tokenService.GetTokenAsync();
+		return account?.Id;
+	}
+	public async Task<List<AccountDto>> GetAccountsAsync()
+		=> await _repository.GetAccountsAsync();
+
+	public async Task<TokenResponse?> LoginAsync(SignInDTOs model)
+	{
+		model.HashPassword = PasswordHasher.HashPasswordPBKDF2(model.HashPassword!);
+		var account = _mapper.Map<Account>(model);
+
+		var result = await _repository.LoginAsync(account);
+		if (result == null)
+			throw new NotFoundException(ErrorMessageBase.NotFound);
+
+
 		var accessToken = _tokenService.GenerateAccessToken(result);
 		var refreshToken = _tokenService.GenerateAccessToken(result);
 		_tokenService.SetTokensInsideCookie(accessToken, refreshToken);
@@ -87,17 +76,6 @@ public class AccountServices(
 			RefreshToken = refreshToken
 		};
 	}
-=======
-        var accessToken = _tokenService.GenerateAccessToken(result);
-        var refreshToken = _tokenService.GenerateAccessToken(result);
-        _tokenService.SetTokensInsideCookie(accessToken, refreshToken);
-        return new TokenResponse
-        {
-            AccessToken = accessToken,
-            RefreshToken = refreshToken
-        };
-    }
->>>>>>> Stashed changes
 
     public async Task<bool> RegisterAsync(SignupDTOs model)
     {
@@ -105,16 +83,9 @@ public class AccountServices(
         account.HashPassword = PasswordHasher.HashPasswordPBKDF2(model.Password);
         account.RoleName = AppRoles.Customer.ToString();
 
-<<<<<<< HEAD
-        _repository.Create(account);
-        if (await _repository.SaveChangesAsync())
-        {
-            return true;
-        }
-        return false;
-    }
-=======
+
 		_repository.Create(account);
+
 
 		var cartModel = new Cart
 		{
@@ -129,7 +100,6 @@ public class AccountServices(
 		}
 		return false;
 	}
->>>>>>> 4f09a67695f7b2273ebc1399972cf08836ee3b3a
 
     public async Task<bool> ResetPasswordAsync(string Email, ForgotPassDTOs model)
     {
@@ -148,9 +118,7 @@ public class AccountServices(
         var existingAccount = await _repository.GetByIdAsync(model.Id);
         if (existingAccount == null)
             throw new NotFoundException(ErrorMessageBase.Format(ErrorMessageBase.NotFound, "Account", model.Id));
-
-<<<<<<< HEAD
-        var oldAvatar = existingAccount.Avatar;
+		var oldAvatar = existingAccount.Avatar;
         var account = _mapper.Map(model, existingAccount);
         if (model.Avatar == null)
         {
@@ -211,55 +179,6 @@ public class AccountServices(
         var existingAccount = await _repository.GetByIdAsync(model.Id);
         if (existingAccount == null) return false;
         existingAccount.RoleName = model.RoleName!;
-=======
-		_repository.Update(account);
-		return await _repository.SaveChangesAsync() ? true : false;
-	}
-	public async Task<bool> UpdatePhoneNumberAsync(Guid accountId, string phoneNumber)
-	{
-		var account = await _repository.GetByIdAsync(accountId);
-		if (account == null)
-			throw new NotFoundException(ErrorMessageBase.Format(ErrorMessageBase.NotFound, "Account", accountId));
-		account.Phone = phoneNumber;
-		_repository.Update(account);
-		return await _repository.SaveChangesAsync() ? true : false;
-	}
-	public async Task<bool> HandleAccountAction([FromBody] AccountActionDto request)
-	{
-		var account = await _repository.GetByIdAsync(request.Id);
-		if (account == null)
-			throw new NotFoundException(ErrorMessageBase.Format(ErrorMessageBase.NotFound, "Account", request.Id));
-
-		if (request.Action == AccountAction.VerifyByPassword.ToString())
-		{
-			return await _repository.VerifyAccountByPasswordAsync(account.Id, PasswordHasher.HashPasswordPBKDF2(request.Password!));
-		}
-		if (request.Action == AccountAction.ChangeNumber.ToString())
-		{
-			account.Phone = request.NewPhoneNumber;
-			_repository.Update(account);
-			return await _repository.SaveChangesAsync() ? true : false;
-		}
-		if (request.Action == AccountAction.ChangeEmail.ToString())
-		{
-			account.Email = request.NewEmail!;
-			_repository.Update(account);
-			return await _repository.SaveChangesAsync() ? true : false;
-		}
-		if (request.Action == AccountAction.ChangePassword.ToString())
-		{
-			account.HashPassword = PasswordHasher.HashPasswordPBKDF2(request.NewPassword!);
-			_repository.Update(account);
-			return await _repository.SaveChangesAsync() ? true : false;
-		}
-		return false;
-	}
-    public async Task<bool> UpdateRoleAsync(AccountDto model)
-	{
-        var existingAccount = await _repository.GetByIdAsync(model.Id);
-        if (existingAccount == null) return false;
-        existingAccount.RoleName = model.RoleName;
->>>>>>> 676201f819531f9ef0262eb1997954c9475d276d
         _repository.Update(existingAccount);
         return await _repository.SaveChangesAsync();
     }
