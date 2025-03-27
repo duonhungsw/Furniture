dd722f5fb097cc4fd8067f92ff585deb1f279e08
ThuHa307 <hatran3072003@gmail.com>
2025-03-27 22:08:43 +0700

    create filter product feature

--- Furniture.Service/Services/Implements/ProductServices.cs
+++ Furniture.Service/Services/Implements/ProductServices.cs
@@ -171,4 +171,89 @@ public class ProductServices(
         var products = await _repository.FilterProductsAsync(filterInfo);
         return _mapper.Map<List<ProductDto>>(products);
     }
+    public async Task<List<ProductDto>> SearchProductsAsync(string keyword)
+    {
+        var products = await _repository.SearchProductsAsync(keyword);
+        return _mapper.Map<List<ProductDto>>(products);
+    }
+    protected string? picture;
+    public async Task<bool> UpdateAsync(ProductDto model)
+    {
+        string containerName = ContainerName.product.ToString();
+        var existingProduct = await _repository.GetByIdAsync(model.Id);
+        if (existingProduct == null)
+            throw new NotFoundException(ErrorMessageBase.Format(ErrorMessageBase.NotFound, "Product", model.Id));
+
+        List<string> newPictureUrls = new List<string>();
+
+        if (model.Images != null && model.Images.Count > 0)
+        {
+            var newImageNames = model.Images.Select(file => file.FileName).ToList();
+            var oldImageNames = existingProduct.PictureUrl?
+                .Split(',')
+                .Select(url => Path.GetFileName(new Uri(url).LocalPath))
+                .ToList() ?? new List<string>();
+
+            bool isSameImages = newImageNames.SequenceEqual(oldImageNames);
+
+            if (isSameImages)
+            {
+                var updated = _mapper.Map(model, existingProduct);
+                updated.PictureUrl = existingProduct.PictureUrl;
+                _repository.Update(updated);
+                await _repository.SaveChangesAsync();
+                return true;
+            }
+            if (!string.IsNullOrEmpty(existingProduct.PictureUrl))
+            {
+                var oldImageUrls = existingProduct.PictureUrl.Split(',');
+
+                foreach (var imageUrl in oldImageUrls)
+                {
+                    var uri = new Uri(imageUrl);
+                    string oldBlobName = Path.GetFileName(uri.LocalPath);
+
+                    bool fileExists = await _storageService.FileExistsAsync(containerName, oldBlobName);
+                    if (fileExists)
+                    {
+                        bool isDeleted = await _storageService.DeleteFileAsync(containerName, oldBlobName);
+                        if (!isDeleted)
+                        {
+                            throw new Exception($"Can't delete old picture: {oldBlobName}");
+                        }
+                    }
+                }
+            }
+            newPictureUrls = await _storageService.SaveFilesAsync(containerName, model.Images);
+        }
+        else
+        {
+            newPictureUrls = existingProduct.PictureUrl?.Split(',').ToList() ?? new List<string>();
+        }
+        model.PictureUrl = string.Join(",", newPictureUrls);
+
+        var updatedProduct = _mapper.Map(model, existingProduct);
+        updatedProduct.PictureUrl = model.PictureUrl;
+        _repository.Update(updatedProduct);
+        await _repository.SaveChangesAsync();
+        return true;
+	}
+
+	public async Task<List<string>> GetBrandAsync()
+	{
+		var brands = await _repository.GetBrandAsync();
+		return brands;
+	}
+
+	public async Task<List<string>> GetTypeAsync()
+	{
+		var types = await _repository.GetTypeAsync();
+		return types;
+	}
+
+    public async Task<List<ProductDto>> FilterProductsAsync(FilterProductInfo filterInfo)
+    {
+        var products = await _repository.FilterProductsAsync(filterInfo);
+        return _mapper.Map<List<ProductDto>>(products);
+    }
 }
