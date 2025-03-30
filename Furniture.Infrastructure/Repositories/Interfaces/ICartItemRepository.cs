a3b3c7d4e0b20de646df1afe56a7f6d71badd211
duonhungsw <ddhung2003@gmail.com>
2025-03-30 18:33:43 +0700

    update create order service

--- Furniture.Infrastructure/Repositories/Interfaces/ICartItemRepository.cs
+++ Furniture.Infrastructure/Repositories/Interfaces/ICartItemRepository.cs
@@ -6,6 +6,9 @@ public interface ICartItemRepository : IGenericRepository<CartItem>
 	Task<bool> AddCartItemIsContainAsync(CartItem cartItem, int quantity);
 	Task<bool> CheckCartItemByProductIdAsync(Cart cart, Guid ProductId);
 	Task<CartItem?> GetCartItemByCartIdAndProductIdAsync(Guid cartId, Guid productId);
-    Task<List<CartItem>?> GetCartItemByUserIdAsync(Guid userId);
-    Task<bool> IsCartItemExistsAsync(Guid productId, Guid accountId);
+	Task<List<CartItem>?> GetCartItemByUserIdAsync(Guid userId);
+	Task<bool> IsCartItemExistsAsync(Guid productId, Guid accountId);
+	Task<List<CartItem>?> GetCartsItemByCartIdAndProductIdAsync(Guid cartId, Guid productId);
+	void DeleteRangeAsync(List<CartItem> cartItems);
+
 }
